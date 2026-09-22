db9e6a60f38dce0dd6aa3957f4ac109514e71b0b
ofoscar <ktaoscar@gmail.com>
2026-09-22 09:06:43 -0600

    ci test simulation

--- tests/test_projects.py
+++ tests/test_projects.py
@@ -1,5 +1,11 @@
 from fastapi.testclient import TestClient
 from ofoscar_backend.main import app
+from ofoscar_backend.routers.auth import get_current_admin
+
+def override_get_current_admin():
+    return "test@example.com"
+
+app.dependency_overrides[get_current_admin] = override_get_current_admin
 
 client = TestClient(app)
 
@@ -16,9 +22,20 @@ def test_create_project():
   assert response.json()["title"] == "Test"
 
 def test_get_projects():
+  client.post(
+        "/projects",
+        json={
+            "title": "Test",
+            "description": "Description test",
+        },
+    )
+
   response = client.get("/projects")
 
   assert response.status_code == 200
-  assert len(response.json()) == 1
-  assert response.json()[0]["title"] == "Test"
+  assert len(response.json()) >= 1
+  assert any(
+        project["title"] == "Test"
+        for project in response.json()
+    )
 
